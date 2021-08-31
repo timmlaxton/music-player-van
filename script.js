829baa4f38f9083b34a39393b5134b7c77f5a363
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-31 17:39:08 +0100

    can go to prev and next tracks

--- script.js
+++ script.js
@@ -37,6 +37,30 @@ function pauseSong() {
   audio.pause();
 }
 
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songindex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
 playBtn.addEventListener("click", () => {
   const isPlaying = musicContainer.classList.contains("play");
 
@@ -46,3 +70,6 @@ playBtn.addEventListener("click", () => {
     playSong();
   }
 });
+
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
