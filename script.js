6f5de289f860f1dd937b32305ad58fc525bb3253
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-31 17:49:56 +0100

    can skip progress bar to different points in timeline

--- script.js
+++ script.js
@@ -53,12 +53,26 @@ function nextSong() {
   songIndex++;
 
   if (songIndex > songs.length - 1) {
-    songindex = 0;
+    songIndex = 0;
   }
 
   loadSong(songs[songIndex]);
 
   playSong();
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
 }
 
 playBtn.addEventListener("click", () => {
@@ -73,3 +87,9 @@ playBtn.addEventListener("click", () => {
 
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+
+audio.addEventListener("timeupdate", updateProgress);
+
+progressContainer.addEventListener("click", setProgress);
+
+audio.addEventListener("ended", nextSong);
