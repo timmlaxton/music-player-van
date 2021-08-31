736b7e74ed8ec4aa6d9f6978e1ffbc1e14650026
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-31 17:27:11 +0100

    can play and pause tracks

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
-const musicContainer = document.getElementById("music_container");
+const musicContainer = document.getElementById("music-container");
 const playBtn = document.getElementById("play");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 
 const audio = document.getElementById("audio");
 const progress = document.getElementById("progress");
-const progressContainer = document.getElementById("progress_container");
+const progressContainer = document.getElementById("progress-container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
+
+const songs = ["Three Little Birds", "Stir It Up", "Waiting In Vain"];
+
+let songIndex = 0;
+
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `img/${song}.jpg`;
+}
+
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
